977721d22a13fc10d46227976d346829a28671e8
thecef02 <thecef@hotmail.com>
2026-05-13 16:47:59 -0600

    05-13-26

---
+++ scripts/prophets.js
@@ -0,0 +1,66 @@
+const url =    "https://byui-cse.github.io/cse-ww-program/data/latter-day-prophets.json";
+const cards = document.querySelector("#cards");
+
+const displayProphets = (prophets) => {
+    prophets.forEach((prophet) => {
+        // Create elements to add to the div.cards element
+        let card = document.createElement("section");
+        let fullName = document.createElement("h2"); // fill in the blank
+        let portrait = document.createElement("img");
+        let birthDate = document.createElement("p");
+        let birthPlace = document.createElement("p");
+        // Build the h2 content out to show the prophet's full name
+        fullName.textContent = `${prophet.name} ${prophet.lastname}`; // fill in the blank
+        // Build the image portrait by setting all the relevant attributes
+        portrait.setAttribute("src", prophet.imageurl);
+        portrait.setAttribute(
+            "alt",
+            `Portrait of  ${prophet.name} ${prophet.lastname} - ${prophet.order}${ordinal_suffix_of(prophet.order)} Latter-day President`);
+        // fill in the blank
+        portrait.setAttribute("loading", "lazy");
+        portrait.setAttribute("width", "340");
+        portrait.setAttribute("height", "440");
+
+        birthDate.innerHTML = `Date of Birth: <strong>${prophet.birthdate}</strong>`;
+        birthPlace.innerHTML = `Place of Birth: <strong>${prophet.birthplace}</strong>`;
+        // Add/append the section(card) with the h2 element
+        card.appendChild(fullName);
+        card.appendChild(birthDate);
+        card.appendChild(birthPlace);
+        card.appendChild(portrait);
+        
+        // Append the section(card) with the created elements
+        // card.appendChild(_______); //fill in the blank
+        card.appendChild(portrait);
+
+        cards.appendChild(card);
+    }); // end of arrow function and forEach loop
+};
+
+async function getProphetData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    // console.table(data.prophets);
+    displayProphets(data.prophets);
+}
+function ordinal_suffix_of(i) {
+    var j = i % 10,
+        k = i % 100;
+    if (j == 1 && k != 11) {
+        return i + "st";
+    }
+    if (j == 2 && k != 12) {
+        return i + "nd";
+    }
+    if (j == 3 && k != 13) {
+        return i + "rd";
+    }
+    return i + "th";
+}
+
+getProphetData();
+
+
+
+
+
