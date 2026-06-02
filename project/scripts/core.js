9529e0663d36d8927786c110b5972efc5e1d6642
thecef02 <thecef@hotmail.com>
2026-06-02 17:45:51 -0600

    06-02-26_3

--- project/scripts/core.js
+++ project/scripts/core.js
@@ -1,26 +1,26 @@
 
 /***************** Navigation ******************/
-// const navButton = document.querySelector("#nav-button");
-// const nav = document.querySelector("nav");
-// const navLinks = document.querySelectorAll("nav a");
+const navButton = document.querySelector("#nav-button");
+const nav = document.querySelector("nav");
+const navLinks = document.querySelectorAll("nav a");
 
-// navButton.addEventListener("click", () => {
-//     navButton.classList.toggle("show");
-//     nav.classList.toggle("show");  
-// });
+navButton.addEventListener("click", () => {
+    navButton.classList.toggle("show");
+    nav.classList.toggle("show");  
+});
 
 
-// navLinks.forEach((link) => {
-//     link.addEventListener("click", () => {
-//         navButton.classList.remove("show");
-//         nav.classList.remove("show");
+navLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+        navButton.classList.remove("show");
+        nav.classList.remove("show");
 
-//         navLinks.forEach((navLink) => {
-//             navLink.parentElement.classList.remove("current");
-//         });
-//         link.parentElement.classList.add("current");
-//     });
-// });
+        navLinks.forEach((navLink) => {
+            navLink.parentElement.classList.remove("current");
+        });
+        link.parentElement.classList.add("current");
+    });
+});
 
 /***************** End Navigation ******************/
 
